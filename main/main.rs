3e4a2f24b460d174d5be74a88630e20482c4f968
DarkEld3r <stanislav.tkach@gmail.com>
2015-10-21 00:30:07 +0300

    TODO comments

--- main/main.rs
+++ main/main.rs
@@ -1,4 +1,24 @@
+//extern crate opengl_graphics;
 
 fn main()
 {
+	/*
+    let opengl = opengl_graphics::OpenGL::_3_2;
+
+    let window: GlutinWindow =
+        WindowSettings::new("Test", [1024, 600])
+            .exit_on_esc(true)
+            .opengl(opengl)
+            .samples(8)
+            .into();
+
+    let mut gl = opengl_graphics::GlGraphics::new(opengl);
+
+    // Event handling
+    for e in window.events().ups(60).max_fps(60) {
+        match e {
+            _ => {}
+        }
+    }
+	*/
 }
