e230f4e1a1ab7c2d4586170f743a96c8fa2dd26b
DarkEld3r <stanislav.tkach@gmail.com>
2015-11-15 01:51:05 +0200

    use piston_window as pw

--- main/main.rs
+++ main/main.rs
@@ -1,13 +1,16 @@
 extern crate piston_window;
 
-use piston_window::{PistonWindow, WindowSettings};
+use piston_window as pw;
 
 fn main() {
-    let window: PistonWindow = WindowSettings::new("rrthozopsi", [600, 600])
+    let window: pw::PistonWindow = pw::WindowSettings::new("rrthozopsi", [600, 600])
         .exit_on_esc(true)
         .build()
         .unwrap();
 
-    for _e in window {
+    for e in window {
+		e.draw_2d(|context, graphics| {
+			pw::clear([0.0, 0.0, 0.0, 1.0], graphics);
+		});
     }
 }
