b9220b04f430f7692fd013945f0736523bafadd6
DarkEld3r <stanislav.tkach@gmail.com>
2015-11-15 01:34:55 +0200

    use piston_window...

--- main/main.rs
+++ main/main.rs
@@ -1,7 +1,9 @@
 extern crate piston_window;
 
+use piston_window::{PistonWindow, WindowSettings};
+
 fn main() {
-    let window: piston_window::PistonWindow = piston_window::WindowSettings::new("rrthozopsi", [600, 600])
+    let window: PistonWindow = WindowSettings::new("rrthozopsi", [600, 600])
         .exit_on_esc(true)
         .build()
         .unwrap();
