33701ee711f747c3476f54187b9a23eff52980f2
DarkEld3r <stanislav.tkach@gmail.com>
2015-11-15 02:21:01 +0200

    rotation

--- main/main.rs
+++ main/main.rs
@@ -10,14 +10,20 @@ fn main() {
         .build()
         .unwrap();
 
+	let mut rotation: f64 = 0.0;
     for w in window {
+		match w.event {
+			Some(pw::Event::Update(pw::UpdateArgs { dt })) => { rotation += 3.0 * dt; }
+			_ => {}
+		}
+
 		w.draw_2d(|context, graphics| {
 			pw::clear([0.0, 0.0, 0.0, 1.0], graphics);
 
 			let red = [1.0, 0.0, 0.0, 1.0];
 			let square = pw::rectangle::square(0.0, 0.0, 100.0);
 			let center = context.transform.trans(300.0, 300.0);
-			pw::rectangle(red, square, center.trans(-50.0, -50.0), graphics);
+			pw::rectangle(red, square, center.rot_rad(rotation).trans(-50.0, -50.0), graphics);
 		});
     }
 }
