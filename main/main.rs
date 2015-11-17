53bad8fb43afee472f288c3a6aef0c31609ef88c
DarkEld3r <stanislav.tkach@gmail.com>
2015-11-18 00:39:07 +0200

    ARguments by reference

--- main/main.rs
+++ main/main.rs
@@ -13,11 +13,11 @@ impl Game {
         Game { rotation : 0.0 }
     }
 
-    fn on_update(&mut self, args: pw::UpdateArgs) {
+    fn on_update(&mut self, args: &pw::UpdateArgs) {
         self.rotation += 3.0 * args.dt;
     }
 
-    fn on_draw(&mut self, args: pw::RenderArgs, window: pw::PistonWindow) {
+    fn on_draw(&mut self, args: &pw::RenderArgs, window: &pw::PistonWindow) {
         window.draw_2d(|context, graphics| {
             pw::clear([0.0, 0.0, 0.0, 1.0], graphics);
 
@@ -39,8 +39,8 @@ fn main() {
 
     for w in window {
 		match w.event {
-			Some(pw::Event::Update(args)) => { game.on_update(args); }
-			Some(pw::Event::Render(args)) => { game.on_draw(args, w); }
+			Some(pw::Event::Update(args)) => { game.on_update(&args); }
+			Some(pw::Event::Render(args)) => { game.on_draw(&args, &w); }
 			_ => {}
 		}
     }
