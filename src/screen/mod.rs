49e74cb00ac8280a2e4450e6af8aae7ee71bae0b
DarkEld3r <stanislav.tkach@gmail.com>
2015-12-21 23:03:30 +0200

    Screen trait membeers

--- src/screen/mod.rs
+++ src/screen/mod.rs
@@ -1,2 +1,7 @@
+use piston_window;
+
 pub trait Screen {
+    fn on_input(&mut self, input: &piston_window::Input);
+    fn on_draw(&mut self, args: &piston_window::RenderArgs, window: &piston_window::PistonWindow);
+    fn on_update(&mut self, args: &piston_window::UpdateArgs);
 }
