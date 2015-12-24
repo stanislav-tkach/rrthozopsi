a6c0d439699026059d78735ca609006bbbcebea7
DarkEld3r <stanislav.tkach@gmail.com>
2015-12-25 01:05:36 +0200

    InputResult enum

--- src/screen/screen.rs
+++ src/screen/screen.rs
@@ -1,5 +1,8 @@
 use piston_window;
 
+enum InputResult {
+}
+
 pub trait Screen {
     fn on_input(&mut self, input: &piston_window::Input);
     fn on_draw(&mut self, args: &piston_window::RenderArgs, window: &piston_window::PistonWindow);
