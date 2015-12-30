bdc37a1b33d317f1e0e79541bd44e79266162885
DarkEld3r <stanislav.tkach@gmail.com>
2015-12-31 01:26:09 +0200

    Battle screen - work in progress

--- src/screen/battle_screen.rs
+++ src/screen/battle_screen.rs
@@ -15,7 +15,32 @@ impl BattleScreen {
 
 impl Screen for BattleScreen {
     fn on_input(&mut self, input: &piston_window::Input) -> InputResults {
+        use piston_window::{Input, Button, Key};
+
         let result = Vec::new();
+
+        match input {
+            &Input::Press(button) => {
+                match button {
+//                    Button::Keyboard(Key::Up) => { self.up = true; }
+//                    Button::Keyboard(Key::Down) => { self.down = true; }
+//                    Button::Keyboard(Key::Left) => { self.left = true; }
+//                    Button::Keyboard(Key::Right) => { self.right = true; }
+                    _ => {}
+                }
+            }
+            &Input::Release(button) => {
+                match button {
+//                    Button::Keyboard(Key::Up) => { self.up = false; }
+//                    Button::Keyboard(Key::Down) => { self.down = false; }
+//                    Button::Keyboard(Key::Left) => { self.left = false; }
+//                    Button::Keyboard(Key::Right) => { self.right = false; }
+                    _ => {}
+                }
+            }
+            _ => {}
+        }
+
         result
     }
 
