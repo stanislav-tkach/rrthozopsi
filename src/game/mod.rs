66006df30db560dab093f3bb4d63db64af6f0506
DarkEld3r <stanislav.tkach@gmail.com>
2016-01-05 00:08:59 +0200

    on_update has been removed from game

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -39,13 +39,6 @@ impl Game {
         }
     }
 
-    pub fn on_update(&mut self, args: &piston_window::UpdateArgs) {
-        if self.events.is_up()   { self.player.mov(0.0, -150.0 * args.dt); }
-        if self.events.is_down() { self.player.mov(0.0, 150.0 * args.dt); }
-        if self.events.is_left() { self.player.mov(-150.0 * args.dt, 0.0); }
-        if self.events.is_right() { self.player.mov(150.0 * args.dt, 0.0); }
-    }
-
     pub fn on_draw(&mut self, args: &piston_window::RenderArgs, window: &piston_window::PistonWindow) {
         window.draw_2d(|context, graphics| {
             piston_window::clear([0.0, 0.0, 0.0, 1.0], graphics);
