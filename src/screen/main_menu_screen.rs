ffbbe82263ba2c7f8de7e2d8e681830e23038ec8
DarkEld3r <stanislav.tkach@gmail.com>
2016-01-17 01:26:09 +0200

    TODO note

--- src/screen/main_menu_screen.rs
+++ src/screen/main_menu_screen.rs
@@ -29,6 +29,25 @@ impl Screen for MainMenuScreen {
     fn on_draw(&mut self, args: &piston_window::RenderArgs, window: &piston_window::PistonWindow) {
         window.draw_2d(|context, graphics| {
             piston_window::clear([0., 0., 1., 1.0], graphics);
+
+// TODO: Some control?
+////////////////////////////////////////////////////////////////////////////////////
+/*
+    Canvas::new()
+        .frame(app.frame_width)
+        .pad(30.0)
+        .color(app.bg_color)
+        .scroll_kids()
+        .set(CANVAS, ui);
+
+    // Text example.
+    Text::new("Widget Demonstration")
+        .top_left_with_margins_on(CANVAS, 0.0, app.title_pad)
+        .font_size(32)
+        .color(app.bg_color.plain_contrast())
+        .set(TITLE, ui);
+*/
+////////////////////////////////////////////////////////////////////////////////////
         });
     }
 
