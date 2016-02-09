cce5bf0b9d3c05e65949f86a1cd940b9841676b6
DarkEld3r <stanislav.tkach@gmail.com>
2016-02-09 22:28:08 +0200

    MainMenu: handle_on_update function has been removed

--- src/screen/main_menu_screen.rs
+++ src/screen/main_menu_screen.rs
@@ -47,7 +47,41 @@ impl Screen for MainMenuScreen {
     }
 
     fn on_update(&mut self, args: &piston_window::UpdateArgs) {
-        handle_on_update(&mut self.ui, &mut self.state);
+        let state = &mut self.state;
+
+        self.ui.set_widgets(|ui| {
+            conrod::Canvas::new()
+                .pad(30.)
+                .color(conrod::color::rgb(0.2, 0.35, 0.45))
+                .scroll_kids()
+                .set(CANVAS, ui);
+
+            conrod::Button::new()
+                .w_h(200.0, 50.0)
+                .mid_left_of(CANVAS)
+                .rgb(0.4, 0.75, 0.6)
+                .label("New game")
+                .react(|| *state = Some(State::NewGame))
+                .set(NEW_GAME_BUTTON, ui);
+
+            conrod::Button::new()
+                .w_h(200.0, 50.0)
+                .mid_left_of(CANVAS)
+                .down_from(NEW_GAME_BUTTON, 45.0)
+                .rgb(0.4, 0.75, 0.6)
+                .label("Options")
+                .react(|| *state = Some(State::Options))
+                .set(OPTIONS_BUTTON, ui);
+
+            conrod::Button::new()
+                .w_h(200.0, 50.0)
+                .mid_left_of(CANVAS)
+                .down_from(OPTIONS_BUTTON, 45.0)
+                .rgb(0.4, 0.75, 0.6)
+                .label("Exit")
+                .react(|| *state = Some(State::Exit))
+                .set(EXIT_BUTTON, ui);
+        });
     }
 }
 
@@ -65,40 +99,4 @@ fn create_ui(window: &piston_window::PistonWindow,
     let theme = conrod::Theme::default();
     let glyph_cache = piston_window::Glyphs::new(&font_path, window.factory.borrow().clone());
     conrod::Ui::new(glyph_cache.unwrap(), theme)
-}
-
-fn handle_on_update(ui: &mut conrod::Ui<piston_window::Glyphs>, state: &mut Option<State>) {
-    ui.set_widgets(|ui| {
-        conrod::Canvas::new()
-            .pad(30.)
-            .color(conrod::color::rgb(0.2, 0.35, 0.45))
-            .scroll_kids()
-            .set(CANVAS, ui);
-
-        conrod::Button::new()
-            .w_h(200.0, 50.0)
-            .mid_left_of(CANVAS)
-            .rgb(0.4, 0.75, 0.6)
-            .label("New game")
-            .react(|| *state = Some(State::NewGame))
-            .set(NEW_GAME_BUTTON, ui);
-
-        conrod::Button::new()
-            .w_h(200.0, 50.0)
-            .mid_left_of(CANVAS)
-            .down_from(NEW_GAME_BUTTON, 45.0)
-            .rgb(0.4, 0.75, 0.6)
-            .label("Options")
-            .react(|| *state = Some(State::Options))
-            .set(OPTIONS_BUTTON, ui);
-
-        conrod::Button::new()
-            .w_h(200.0, 50.0)
-            .mid_left_of(CANVAS)
-            .down_from(OPTIONS_BUTTON, 45.0)
-            .rgb(0.4, 0.75, 0.6)
-            .label("Exit")
-            .react(|| *state = Some(State::Exit))
-            .set(EXIT_BUTTON, ui);
-    });
 }
