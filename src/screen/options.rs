727db6106d27e0195360d171c94ec17a6f73c48f
DarkEld3r <stanislav.tkach@gmail.com>
2016-02-15 01:26:43 +0200

    Options struct

--- src/screen/options.rs
+++ src/screen/options.rs
@@ -2,3 +2,8 @@ use screen::*;
 use piston_window::{self, PistonWindow};
 use conrod::{self, Widget, Positionable, Sizeable, Labelable};
 use conrod::color::Colorable;
+
+pub struct Options {
+    ui: conrod::Ui<piston_window::Glyphs>,
+    back: bool,
+}
