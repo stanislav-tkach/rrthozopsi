d2f6670bca357fbfb8163ad5e647f43c70b0447f
DarkEld3r <stanislav.tkach@gmail.com>
2016-02-15 02:02:03 +0200

    Options screen

--- src/screen/options.rs
+++ src/screen/options.rs
@@ -12,7 +12,56 @@ impl Options {
     pub fn new(window: &PistonWindow, context: &mut Context) -> Self {
         MainMenu {
             ui: ui_utils::create_ui(&window, &context.assets_path),
-            state: None,
+            back: false,
         }
     }
+}
+
+impl Screen for MainMenu {
+    fn on_input(&mut self,
+                _: &piston_window::Input,
+                window: &PistonWindow,
+                context: &mut Context)
+                -> InputResults {
+        self.ui.handle_event(window);
+        let mut result = Vec::new();
+
+        if self.back {
+            result.push(InputResult::PopScreen);
+        }
+
+        result
+    }
+
+    fn on_draw(&mut self, _: &piston_window::RenderArgs, window: &PistonWindow) {
+        self.ui.handle_event(window);
+        window.draw_2d(|context, graphics| self.ui.draw_if_changed(context, graphics));
+    }
+
+    fn on_update(&mut self, _: &piston_window::UpdateArgs) {
+        let button_color = conrod::color::rgb(0.4, 0.75, 0.6);
+
+        let back = &mut self.state;
+
+        self.ui.set_widgets(|ui| {
+            conrod::Canvas::new()
+                .pad(30.)
+                .color(conrod::color::rgb(0.2, 0.35, 0.45))
+                .scroll_kids()
+                .set(CANVAS, ui);
+
+            conrod::Button::new()
+                .w_h(200.0, 50.0)
+                .mid_left_of(CANVAS)
+                .color(button_color)
+                .label("Back")
+                .react(|| *back = true)
+                .set(BACK_BUTTON, ui);
+        });
+    }
+}
+
+widget_ids! {
+    CANVAS,
+    BACK_BUTTON,
 }
