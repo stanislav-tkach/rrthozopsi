dbf8e85db50caa541405967cc94a1bd59a5d1947
DarkEld3r <stanislav.tkach@gmail.com>
2016-03-31 01:11:56 +0300

    'private type in public interface' warning fix

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -2,7 +2,7 @@ use std::path::Path;
 use piston_window::{PistonWindow, G2d, Glyphs};
 use conrod::{self, Theme, Graphics};
 
-type Backend = (<G2d<'static> as Graphics>::Texture, Glyphs);
+pub type Backend = (<G2d<'static> as Graphics>::Texture, Glyphs);
 pub type Ui = conrod::Ui<Backend>;
 
 pub fn create_ui(window: &PistonWindow, font_path: &Path) -> Ui {
